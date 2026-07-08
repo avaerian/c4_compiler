2d03ef1b5d71ac6c29a4dc04af5db22c31fc488c
avaerian <avaeriandev@gmail.com>
2026-07-08 13:08:48 -0400

    Refactor structs for token variants as anonymous structs for cleanup

--- lex.h
+++ lex.h
@@ -33,20 +33,6 @@ typedef struct lexer {
     uint32_t col;
 } lexer_t;
 
-typedef struct token_sym {
-    //sym_kind_t sym;
-    token_kind_t _unused; //FIXME
-} token_sym_t;
-
-typedef struct token_comment {
-    const char* comment;
-    uint32_t len;
-} token_comment_t;
-
-typedef struct token_ident {
-    const char* ident; //TODO: use symbol table instead of copying each ident
-} token_ident_t;
-
 typedef enum kw {
     FOR = 0, 
     WHILE, 
@@ -70,17 +56,25 @@ typedef enum kw {
     KEYWORD_LEN // not a keyword; used purely for retrieving size
 } kw_t;
 
-typedef struct token_kw {
-     kw_t kw;
-} token_kw_t;
-
 typedef struct token {
     token_kind_t kind; // type to identify union
     union {
-        token_sym_t sym;
-        token_ident_t ident;
-        token_comment_t comment;
-        token_kw_t keyword;
+        struct {
+            char _unused;
+        } sym;
+
+        struct {
+            const char* ident; //TODO: use symbol table instead of copying each ident
+        } ident;
+
+        struct {
+            const char* comment;
+            uint32_t len;
+        } comment;
+        
+        struct {
+            kw_t kw;
+        } keyword;
     } data; 
     uint32_t row, col;
 } token_t;
